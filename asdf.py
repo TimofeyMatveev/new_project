cb2f7949fc5df348c5c043de44e37d508d2c6541
TimafeyMatveev <tima-7897@mail.ru>
2024-01-21 20:34:33 +0300

    ДОбавил class sadsadsa

--- asdf.py
+++ asdf.py
@@ -3,10 +3,12 @@ class A:
         self.name = name
         self.age = age
 
+
 class B:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
 
 class C:
     def __init__(self, name, age):
