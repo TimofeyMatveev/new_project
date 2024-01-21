1d9ce1169d225038f66f13d736e43de816d28a4a
TimafeyMatveev <tima-7897@mail.ru>
2024-01-21 20:13:56 +0300

    ДОбавил класс B

--- asdf.py
+++ asdf.py
@@ -2,3 +2,8 @@ class A:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+class B:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
